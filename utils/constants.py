7272d065ac9badc28975adcceb25a40aad3abbb3
Lotfi Kobrosly <lotfi@pcinv20231212>
2025-11-24 16:06:03 +0100

    Create utils/constants.py

---
+++ utils/constants.py
@@ -0,0 +1,31 @@
+# Author: Lotfi Kobrosly (2025)
+
+# Constants used accross the repository
+
+# Planets' masses in kilograms (Wikipedia)
+SUN_MASS = 1.9884e30
+MERCURY_MASS = 3.301e23
+VENUS_MASS = 4.867e24
+EARTH_MASS = 5.972e24
+MARS_MASS = 6.417e23
+JUPITER_MASS = 1.899e27
+SATURN_MASS = 5.685e26
+URANUS_MASS = 8.682e25
+NEPTUNE_MASS = 1.024e26
+
+# Dwarf planets and asteroids' masses in kilograms (Wikipedia)
+PLUTO_MASS = 1.471e22
+CERES_MASS = 9.3e20
+VESTA_MASS = 2.6e20
+PALLAS_MASS = 2.0e20
+
+# Planets' satellites' masses in kilograms (Wikipedia)
+MOON_MASS = 7.348e22
+IO_MASS = 8.93e22
+EUROPA_MASS = 4.80e22
+GANYMEDE_MASS = 1.48e23
+CALLISTO_MASS = 1.08e23
+TITAN_MASS = 1.35e23
+TITANIA_MASS = 3.52e21
+OBERON_MASS = 3.01e21
+TRITON_MASS = 2.14e22
